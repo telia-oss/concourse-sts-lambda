a89bfa9fb339930bb6809a093be7bc7233a64819
itsdalmo <kristian@doingit.no>
2018-07-11 20:59:54 +0200

    Rewrite model tests.

--- models_test.go
+++ models_test.go
@@ -2,15 +2,22 @@ package handler_test
 
 import (
 	"encoding/json"
+	"reflect"
 	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/telia-oss/concourse-sts-lambda"
 )
 
 func TestConfig(t *testing.T) {
-	input := strings.TrimSpace(`
+	tests := []struct {
+		description string
+		input       string
+		expected    handler.Team
+	}{
+		{
+			description: "Unmarshal works as intended",
+			input: strings.TrimSpace(`
 {
     "name": "team",
     "accounts": [
@@ -20,45 +27,77 @@ func TestConfig(t *testing.T) {
 	}
     ]
 }
-`)
-
-	t.Run("Unmarshal works as intended", func(t *testing.T) {
-		expected := handler.Team{
-			Name: "team",
-			Accounts: []*handler.Account{
-				{
-					Name:    "account",
-					RoleArn: "role",
+`),
+			expected: handler.Team{
+				Name: "team",
+				Accounts: []*handler.Account{
+					{
+						Name:    "account",
+						RoleArn: "role",
+					},
 				},
 			},
-		}
+		},
+	}
 
-		var actual handler.Team
-		err := json.Unmarshal([]byte(input), &actual)
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			var output handler.Team
+			err := json.Unmarshal([]byte(tc.input), &output)
 
-		assert.Nil(t, err)
-		assert.Equal(t, expected, actual)
-	})
+			if err != nil {
+				t.Fatalf("unexpected error: %s", err)
+			}
+
+			if got, want := output, tc.expected; !reflect.DeepEqual(got, want) {
+				t.Errorf("\ngot:\n%v\nwant:\n%v\n", got, want)
+			}
+		})
+	}
 }
 
 func TestSecretPath(t *testing.T) {
-	var (
-		team    = "TEAM"
-		account = "ACCOUNT"
-	)
+	tests := []struct {
+		description string
+		template    string
+		team        string
+		account     string
+		expected    string
+		shouldError bool
+	}{
+		{
+			description: "template works as intended",
+			template:    "/concourse/{{.Team}}/{{.Account}}",
+			team:        "TEAM",
+			account:     "ACCOUNT",
+			expected:    "/concourse/TEAM/ACCOUNT",
+			shouldError: false,
+		},
+		{
+			description: "fails if the template expects more parameters",
+			template:    "/concourse/{{.Team}}/{{.Account}}/{{.Something}}",
+			team:        "TEAM",
+			account:     "ACCOUNT",
+			expected:    "",
+			shouldError: true,
+		},
+	}
 
-	t.Run("Secret template works as intended", func(t *testing.T) {
-		template := "/concourse/{{.Team}}/{{.Account}}"
-		expected := "/concourse/TEAM/ACCOUNT"
-		actual, err := handler.NewSecretPath(team, account, template).String()
-		assert.Nil(t, err)
-		assert.Equal(t, expected, actual)
-	})
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			got, err := handler.NewSecretPath(tc.team, tc.account, tc.template).String()
 
-	t.Run("Fails if template expects additional parameters", func(t *testing.T) {
-		template := "/concourse/{{.Team}}/{{.Account}}/{{.Something}}"
-		actual, err := handler.NewSecretPath(team, account, template).String()
-		assert.NotNil(t, err)
-		assert.Equal(t, "", actual)
-	})
+			if tc.shouldError && err == nil {
+				t.Fatal("expected an error to occur")
+			}
+
+			if !tc.shouldError && err != nil {
+				t.Fatalf("unexpected error: %s", err)
+			}
+
+			if want := tc.expected; got != want {
+				t.Errorf("\ngot:\n%v\nwant:\n%v\n", got, want)
+			}
+		})
+	}
 }
