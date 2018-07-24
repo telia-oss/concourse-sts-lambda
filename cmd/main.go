036e6bfa8a6930d9ddf645da66be2d7351b80270
itsdalmo <kristian@doingit.no>
2018-07-25 00:16:59 +0200

    Fix JSON logging for initalization as well.

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws/session"
 	flags "github.com/jessevdk/go-flags"
@@ -15,20 +13,24 @@ type Command struct {
 	Path string `long:"secrets-manager-path" env:"SECRETS_MANAGER_PATH" default:"/concourse/{{.Team}}/{{.Account}}" description:"Path to use when writing to AWS Secrets manager."`
 }
 
+var logger *logrus.Logger
+
+func init() {
+	logger = logrus.New()
+	logger.Formatter = &logrus.JSONFormatter{}
+}
+
 func main() {
 	var command Command
 
 	_, err := flags.Parse(&command)
 	if err != nil {
-		panic(fmt.Errorf("failed to parse flag %s", err))
+		logger.Fatalf("failed to parse flag: %s", err)
 	}
 	sess, err := session.NewSession()
 	if err != nil {
-		panic(fmt.Errorf("failed to create new session: %s", err))
+		logger.Fatalf("failed to create a new session: %s", err)
 	}
-
-	logger := logrus.New()
-	logger.Formatter = &logrus.JSONFormatter{}
 
 	f := handler.New(handler.NewManager(sess), command.Path, logger)
 	lambda.Start(f)
