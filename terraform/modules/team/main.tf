5c2acccf4e7319c5bf0190acc558904340f9d877
itsdalmo <kristian@doingit.no>
2019-08-05 14:14:41 +0200

    Update team module

--- terraform/modules/team/main.tf
+++ terraform/modules/team/main.tf
@@ -1,21 +1,10 @@
 # ------------------------------------------------------------------------------
 # Resources
 # ------------------------------------------------------------------------------
-locals {
-  team_config = <<EOF
-  {
-    "name": "${var.name}",
-    "accounts": ${jsonencode(var.accounts)}
-  }
-  EOF
-
-  config_hash = "${substr(md5(local.team_config), 0, 7)}"
-}
-
 resource "aws_s3_bucket_object" "main" {
-  bucket  = "${var.config_bucket}"
+  bucket  = var.config_bucket
   key     = "${var.name}.json"
-  content = "${local.team_config}"
+  content = local.team_config
 }
 
 resource "aws_cloudwatch_event_rule" "main" {
@@ -24,22 +13,33 @@ resource "aws_cloudwatch_event_rule" "main" {
   schedule_expression = "rate(30 minutes)"
 }
 
+resource "aws_lambda_permission" "main" {
+  statement_id  = "concourse-${var.name}-sts-lambda-permission"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_arn
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.main.arn
+}
+
 resource "aws_cloudwatch_event_target" "main" {
-  rule = "${aws_cloudwatch_event_rule.main.name}"
-  arn  = "${var.lambda_arn}"
+  rule = aws_cloudwatch_event_rule.main.name
+  arn  = var.lambda_arn
 
   input = <<EOF
   {
     "bucket": "${var.config_bucket}",
     "key": "${aws_s3_bucket_object.main.key}"
   }
-  EOF
+EOF
 }
 
-resource "aws_lambda_permission" "main" {
-  statement_id  = "concourse-${var.name}-sts-lambda-permission"
-  action        = "lambda:InvokeFunction"
-  function_name = "${var.lambda_arn}"
-  principal     = "events.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_event_rule.main.arn}"
+
+locals {
+  team_config = <<EOF
+  {
+    "name": "${var.name}",
+    "accounts": ${jsonencode(var.accounts)}
+  }
+EOF
+  config_hash = substr(md5(local.team_config), 0, 7)
 }
